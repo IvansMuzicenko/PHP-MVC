dadf93f4484097089d5424276bd5be5c33a69b4a
IvansMuzicenko <ivans.muzicenko@gmail.com>
2022-06-14 10:02:29 +0300

    feat: init

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASS = '';
+const DB_NAME = 'php_mvc';
+
+const APPROOT = dirname(dirname(__FILE__));
+
+const URLROOT = 'localhost/PHP_MVC';
+
+const SITENAME = 'PHP MVC';

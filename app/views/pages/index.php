adbe4d9df65f9f78e5397bdea4ee6630f7c41f3a
IvansMuzicenko <ivans.muzicenko@gmail.com>
2022-06-17 12:41:16 +0300

    feat: basic front-end

---
+++ app/views/pages/index.php
@@ -0,0 +1,14 @@
+<?php
+require APPROOT . '/views/includes/head.php';
+?>
+
+<div id="section-landing">
+    <?php
+    require APPROOT . '/views/includes/navigation.php';
+    ?>
+
+    <div class="wrapper-landing">
+        <h1>Hello and welcome</h1>
+        <h2>to the MVC website</h2>
+    </div>
+</div>

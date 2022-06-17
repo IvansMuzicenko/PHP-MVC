5140d2b90a2597138ede394b33d0ec892e5802c6
IvansMuzicenko <ivans.muzicenko@gmail.com>
2022-06-17 13:55:45 +0300

    feat: login and logout

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -82,4 +82,72 @@ class Users extends Controller
         }
         $this->view('users/register', $data);
     }
+
+    public function login()
+    {
+        $data = [
+            'title' => 'Login Page',
+            'username' => '',
+            'password' => '',
+            'usernameError' => '',
+            'passwordError' => '',
+        ];
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                "username" => trim($_POST['username']),
+                "password" => trim($_POST['password']),
+                'usernameError' => '',
+                'passwordError' => '',
+            ];
+
+            if (empty($data['username'])) {
+                $data['usernameError'] = 'Please enter a username.';
+            }
+
+            if (empty($data['password'])) {
+                $data['passwordError'] = 'Please enter a password.';
+            }
+
+            if (empty($data['usernameError']) && empty($data['passwordError'])) {
+                $loggedInUser = $this->userModel->login($data['username'], $data['password']);
+
+                if ($loggedInUser) {
+                    $this->createUserSession($loggedInUser);
+                } else {
+                    $data['passwordError'] = 'Password or username is incorrect.';
+
+                    $this->view('users/login', $data);
+                }
+            }
+        } else {
+            $data = [
+                'username' => '',
+                'password' => '',
+                'usernameError' => '',
+                'passwordError' => ''
+            ];
+        }
+        $this->view('users/login', $data);
+    }
+
+    public function createUserSession($user)
+    {
+        $_SESSION['user_id'] = $user->id;
+        $_SESSION['email'] = $user->email;
+        $_SESSION['username'] = $user->username;
+
+        header('location: ' . URLROOT . '/pages/index');
+    }
+
+    public function logout()
+    {
+        unset($_SESSION['user_id']);
+        unset($_SESSION['email']);
+        unset($_SESSION['username']);
+
+        header('location: ' . URLROOT . '/users/login');
+    }
 }
--- app/models/User.php
+++ app/models/User.php
@@ -25,6 +25,22 @@ class User
         }
     }
 
+    public function login($username, $password)
+    {
+        $this->db->query('SELECT * FROM users WHERE username = :username');
+        $this->db->bind(':username', $username);
+
+        $row = $this->db->single();
+
+        $hashedPassword = $row->password;
+
+        if (password_verify($password, $hashedPassword)) {
+            return $row;
+        } else {
+            return false;
+        }
+    }
+
     // Find an existence of an user by email
     // Returns boolean
     public function findUserByEmail($email)

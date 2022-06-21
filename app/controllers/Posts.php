6291f26b7f62f5d9d4b45054d19842e73c007aa3
IvansMuzicenko <ivans.muzicenko@gmail.com>
2022-06-21 17:34:29 +0300

    feat: post update

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -58,4 +58,63 @@ class Posts extends Controller
             }
         }
     }
+
+    public function update($id)
+    {
+
+        $post = $this->postModel->findPostById($id);
+
+        if (!isLoggedIn()) {
+            header("Location: " . URLROOT . "/posts");
+        } elseif ($post->user_id != $_SESSION['user_id']) {
+            header("Location: " . URLROOT . "/posts");
+        }
+
+        $data = [
+            'post' => $post,
+            'title' => '',
+            'body' => '',
+            'titleError' => '',
+            'bodyError' => ''
+        ];
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'id' => $id,
+                'post' => $post,
+                'user_id' => $_SESSION['user_id'],
+                'title' => trim($_POST['title']),
+                'body' => trim($_POST['body']),
+                'titleError' => '',
+                'bodyError' => ''
+            ];
+
+            if (empty($data['title'])) {
+                $data['titleError'] = 'Post title must not be empty';
+            }
+
+            if (empty($data['body'])) {
+                $data['bodyError'] = 'Post text must not be empty';
+            }
+
+            if ($data['title'] == $this->postModel->findPostById($id)->title && $data['body'] == $this->postModel->findPostById($id)->body) {
+                $data['titleError'] == 'Title or body must have at least one change';
+                $data['bodyError'] == 'Title or body must have at least one change';
+            }
+
+            if (empty($data['titleError']) && empty($data['bodyError'])) {
+                if ($this->postModel->updatePost($data)) {
+                    header("Location: " . URLROOT . "/posts");
+                } else {
+                    die("Post update failed.");
+                }
+            } else {
+                $this->view('posts/update', $data);
+            }
+        }
+
+        $this->view('posts/update', $data);
+    }
 }
